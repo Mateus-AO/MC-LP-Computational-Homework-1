91bd2ef21da28814a54f75507bd282fc8d81e25b
marceloavlopes <al.marcelo.lopes@impatech.edu.br>
2026-09-20 21:57:49 -0300

    Remoção de comentários

--- Codigos/instancia_matricula.m
+++ Codigos/instancia_matricula.m
@@ -1,6 +1,3 @@
-% GERAR_INSTANCIA_CRITICA.M
-% Script para gerar a instância de teste onde o Simplex ingênuo falha.
-
 clc; clear;
 
 d1 = 3;
@@ -24,8 +21,3 @@ c = [-(d1+1); -(d2+2); -(d3+1); 0; 0; 0];
 [m, n] = size(A);
 
 save('instancia_matricula.mat', 'A', 'b', 'c', 'm', 'n');
-
-% disp('Arquivo "instancia_critica.mat" gerado com sucesso!');
-% disp('Para testar a falha no Octave execute:');
-% disp('  load("instancia_critica.mat");');
-% disp('  [ind, x, d] = simplex_fase1_fase2(A, b, c, m, n);');
